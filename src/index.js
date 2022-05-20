0e0678dabc990047ddc7a43e84737f4934ab3e30
Masih-Saldanha <masih.saldanha@gmail.com>
2022-05-20 18:54:32 -0300

    feat: get randomized questions from database

--- src/index.js
+++ src/index.js
@@ -11,6 +11,25 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+function randomizer() {
+    return Math.random() - 0.5;
+}
+
+app.get("/questions", async (req, res) => {
+    try {
+        const questions = await db.collection("questions").find().toArray();
+
+        questions.sort(randomizer);
+        
+        questions.forEach(q => q.answers.sort(randomizer));
+
+        res.send(questions);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Connected and listening in the port ${process.env.PORT || 5000}`);
 })
